efcf79f9263c4ce5e771177dbe39ee4782d40cac
arraykeys@gmail.com <arraykeys@gmail.com>
2018-04-27 12:00:43 +0800

    Signed-off-by: arraykeys@gmail.com <arraykeys@gmail.com>

--- release_linux.sh
+++ release_linux.sh
@@ -7,6 +7,6 @@ cp conf/autostart goproxy-jui-linux/conf/
 cp -R dist/lib dist/GoProxy.jar  goproxy-jui-linux/
 cp start.sh goproxy-jui-linux/
 cp -R jre_linux goproxy-jui-linux/jre
-7z a  goproxy-jui-linux.7z goproxy-jui-linux
+7z a  -t7z goproxy-jui-linux.7z goproxy-jui-linux  -mx=9 -ms=200m -mf -mhc -mhcf -mmt -r
 
 rm -rf goproxy-jui-linux
--- release_windows.sh
+++ release_windows.sh
@@ -8,7 +8,7 @@ cp -R dist/lib dist/GoProxy.jar  goproxy-jui-windows/
 cp start.vbs bootstrap.bat goproxy-jui-windows/
 cp -R jre_win goproxy-jui-windows/jre
 cp ieshims.dll goproxy-jui-windows/
-7z a goproxy-jui-windows.7z goproxy-jui-windows
+7z a  -t7z goproxy-jui-windows.7z goproxy-jui-windows  -mx=9 -ms=200m -mf -mhc -mhcf -mmt -r
 
 rm -rf goproxy-jui-windows
 
